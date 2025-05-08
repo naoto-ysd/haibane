a8d40b5634cc63228598f248afc7e9bd4e153cb0
“吉田直人” <na-yoshida@m3career.com>
2025-05-08 20:58:57 +0900

    create show hello world using backend

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer /> -->
+    <button @click="fetchHello">Fetch Hello</button>
+    <p>{{ message }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const message = ref('')
+const fetchHello = async () => {
+  message.value = await $fetch('/api/hello')
+}
+</script>
